krexus/vendor_krexus

Backuptool Support (1/2)

products/common.mk
@@ -46,3 +46,9 @@ PRODUCT_COPY_FILES += \
 ifeq ($(PRODUCT_PREBUILT_WEBVIEWCHROMIUM),yes)
 -include prebuilts/chromium/$(TARGET_DEVICE)/chromium_prebuilt.mk
 endif
+
+# Backuptool Support
+PRODUCT_COPY_FILES += \
+    vendor/icarus/prebuilt/common/addon.d/50-icarus.sh:system/addon.d/50-icarus.sh \
+    vendor/icarus/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/icarus/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \

knmurthy/vendor_androidx

config/common.mk
@@ -59,3 +59,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGE_OVERLAYS += vendor/androidx/overlay/common
 
 -include vendor/androidx/sepolicy/sepolicy.mk
+
+# Backuptool Support
+PRODUCT_COPY_FILES += \
+    vendor/androidx/prebuilt/common/addon.d/50-icarus.sh:system/addon.d/50-icarus.sh \
+    vendor/androidx/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/androidx/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \