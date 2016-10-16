18303f2875901e90d7c22e6e459da0f2994000d6
George G <kreach3r@users.noreply.github.com>
2016-10-16 19:04:53 +0700

    (Try to) enable Google Assistant

--- config/common.mk
+++ config/common.mk
@@ -8,6 +8,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.opa.eligible_device=true \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false
